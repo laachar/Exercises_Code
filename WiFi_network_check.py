d119ee767e519827a19c4218b3f6ca36feb21c3f
laachar <37368795+laachar@users.noreply.github.com>
2024-02-06 13:45:36 +0200

    Delete WiFi_network_check.py

--- WiFi_network_check.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-#return all the available network
-networks = subprocess.check_output(['netsh', 'wlan', 'show', 'network'])
-
-decoded_networks = networks.decode('ascii')
-
-print(decoded_networks)
